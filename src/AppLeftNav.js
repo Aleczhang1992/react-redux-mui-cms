6e494b0d96c120b96141803010cd5eb224145f7a
aleczhang <aleczhang@163.com>
2016-11-09 14:54:34 +0800

    提交1.0版本

--- src/AppLeftNav.js
+++ src/AppLeftNav.js
@@ -18,9 +18,7 @@ let styles={
 	}
 };
 
-function handleItemTap(itemId){
-	console.log('商品浏览：'+itemId);
-}
+
 
 export default class AppLeftNav extends Component {	
 	constructor(props) {
@@ -41,7 +39,7 @@ export default class AppLeftNav extends Component {
                         <ListItem key={3} primaryText="统计" value="/news/stats"/>,
 					]}/>
 					<ListItem key={101} primaryText="活动管理" primaryTogglesNestedList={true} nestedItems={[
-						<ListItem key={1} primaryText="浏览" onTouchTap={()=>{handleItemTap('a1234')}} />,
+						<ListItem key={1} primaryText="浏览"  />,
                         <ListItem key={3} primaryText="批量导入" />,
 					]}/>
 					<ListItem key={103} primaryText="设置" />
